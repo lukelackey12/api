7056b39c5e65548882e3514a496bffe8f28faa57
Luke <Luke_Lackey1@baylor.edu>
2024-04-16 18:18:08 -0500

    yes!

--- 1_python_repos_start_file.py
+++ 1_python_repos_start_file.py
@@ -1,7 +1,73 @@
 import requests
+import json
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
+
+outfile = open('output.json', 'w')
+
+response_dict = r.json()
+
+json.dump(response_dict,outfile,indent=4)
+
+#print out how many repos are in this file
+list_of_repos = response_dict['items']
+
+print(type(list_of_repos))
+
+print(len(list_of_repos))
+
+first_repo = list_of_repos[0]
+
+#print out the number of keys in first_repo
+print(f"Number of keys: {len(first_repo)}")
+
+for key in first_repo:
+    print(key)
+
+# Exercise
+# print out the full name, the url, the license name and topics for the first repo
+
+print(f"Full name: {first_repo['full_name']}")
+print(f"URL: {first_repo['owner']['html_url']}")
+print(f'License Name: {first_repo['license']['name']}')
+for topic in first_repo['topics']:
+    print(f"Topic: {topic}")
+
+repo_names, stars = [], []
+
+for repo in list_of_repos[:10]:
+    repo_names.append(repo['name'])
+    stars.append(repo['stargazers_count'])
+
+from plotly.graph_objs import bar
+from plotly import offline
+
+data = [
+    {
+        "type":'bar',
+        "x":repo_names,
+        "y":stars,
+        "marker": {
+            "color":"rgb(60, 100, 150)",
+            "line":{"width":1.5, "color": "rgb(25,25,25)"},
+        },
+        "opacity":0.6,
+    }
+]
+
+my_layout = {
+    "title": "Most Starred Pyton Projects on GitHub",
+    "xaxis":{"title":"Repository"},
+    "yaxis":{"title":"Stars"}
+    
+}
+
+fig = {"data":data, "layout":my_layout}
+
+offline.plot(fig, filename="python_repos.html")
+
+
--- 2_hn_article_start_file.py
+++ 2_hn_article_start_file.py
@@ -7,3 +7,36 @@ r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
 # Explore the structure of the data.
+
+outfile = open('hn.json', 'w')
+
+json.dump(r.json(),outfile,indent=4)
+
+submission_ids = r.json()
+
+url = 'https://hacker-news.firebaseio.com/v0/item/40053774.json'
+r = requests.get(url)
+
+outfile = open('hn2.json','w')
+
+json.dump(r.json(), outfile, indent=4)
+
+response_dict = r.json()
+
+print(f"Title: {response_dict['title']}")
+print(f"URL: {response_dict['url']}")
+print(f"Comments: {response_dict['descendants']}")
+
+
+for subid in submission_ids[:10]:
+    url = f'https://hacker-news.firebaseio.com/v0/item/{subid}.json'
+    r = requests.get(url)
+    response_dict = r.json()
+    print(f"Title: {response_dict['title']}")
+    print(f"URL: {response_dict['url']}")
+    if 'descendants' in response_dict:
+        print(f"Comments: {response_dict['descendants']}")
+
+    print()
+    print()
+
